e835bbd1f932ea101af665118c941665f197e4b3
tracer-nju <tracer@nju.edu.cn>
2025-03-09 13:21:35 +0800

    Linux Dustfire 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:21:35 up  1:25,  0 users,  load average: 0.08, 0.03, 0.01

--- .shadow/labyrinth/labyrinth.c
+++ .shadow/labyrinth/labyrinth.c
@@ -78,7 +78,7 @@ int main(int argc, char *argv[]) {
             }
           }
         }
-        printf("%d", labyrinth->rows);
+        printf("%s", labyrinth->map[0]);
         printf("map  test\n");
 
       } else {
