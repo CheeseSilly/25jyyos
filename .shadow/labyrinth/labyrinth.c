9f55e590c9154406e504aceada428a03e7b7e15d
tracer-nju <tracer@nju.edu.cn>
2025-03-09 13:37:47 +0800

    Linux Dustfire 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:37:47 up  1:41,  0 users,  load average: 0.00, 0.01, 0.00

--- .shadow/labyrinth/labyrinth.c
+++ .shadow/labyrinth/labyrinth.c
@@ -85,7 +85,7 @@ int main(int argc, char *argv[]) {
         perror("args is too many!");
         return 1;
       }
-      printf("%s\n", optarg);
+      printf("%c\n", optarg[0]);
       if (isValidPlayer(optarg[0])) {
         player = optarg[0];
         player_flag = 1;
